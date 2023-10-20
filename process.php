d87c1f40b1b3235e246d340afcad924f3d281582
unknown <sunnysunny621513@gmail.com>
2023-10-20 21:41:19 +0530

    Initial Commit

---
+++ process.php
@@ -0,0 +1,89 @@
+<?php
+print_r($_POST);
+session_start();
+/*
+Note : It is recommended to fetch all the parameters from your Database rather than posting static values or entering them on the UI.
+
+POST REQUEST to be posted to below mentioned PayU URLs:
+
+For PayU Test Server:
+POST URL: https://test.payu.in/_payment
+
+For PayU Production (LIVE) Server:
+POST URL: https://secure.payu.in/_payment
+*/
+
+//Unique merchant key provided by PayU along with salt. Salt is used for Hash signature 
+//calculation within application and must not be posted or transfered over internet. //-->
+$key="ZPepiS71";
+$salt="21Svv0vP8o";
+
+$action = 'https://secure.payu.in/_payment';
+
+$html='';
+
+if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') == 0){
+	/* Request Hash
+	----------------
+	For hash calculation, you need to generate a string using certain parameters 
+	and apply the sha512 algorithm on this string. Please note that you have to 
+	use pipe (|) character as delimeter. 
+	The parameter order is mentioned below:
+	
+	sha512(key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5||||||SALT)
+	
+	Description of each parameter available on html page as well as in PDF.
+	
+	Case 1: If all the udf parameters (udf1-udf5) are posted by the merchant. Then,
+	hash=sha512(key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5||||||SALT)
+	
+	Case 2: If only some of the udf parameters are posted and others are not. For example, if udf2 and udf4 are posted and udf1, udf3, udf5 are not. Then,
+	hash=sha512(key|txnid|amount|productinfo|firstname|email||udf2||udf4|||||||SALT)
+
+	Case 3: If NONE of the udf parameters (udf1-udf5) are posted. Then,
+	hash=sha512(key|txnid|amount|productinfo|firstname|email|||||||||||SALT)
+	
+	In present kit and available PayU plugins UDF5 is used. So the order is -	
+	hash=sha512(key|txnid|amount|productinfo|firstname|email|||||udf5||||||SALT)
+	
+	*/
+	//generate hash with mandatory parameters and udf5
+	$hash=hash('sha512', $key.'|'.$_POST['txnid'].'|'.$_POST['amount'].'|'.$_POST['productinfo'].'|'.$_POST['firstname'].'|'.$_POST['email'].'|||||'.$_POST['udf5'].'||||||'.$salt);
+		
+	$_SESSION['salt'] = $salt; //save salt in session to use during Hash validation in response
+	
+	$html = '<form action="'.$action.'" id="payment_form_submit" method="post">
+			<input type="hidden" id="udf5" name="udf5" value="'.$_POST['udf5'].'" />
+			<input type="hidden" id="surl" name="surl" value="http://localhost/preet2/surl.html" />
+			<input type="hidden" id="furl" name="furl" value="http://localhost/preet2/furl.html" />
+			<input type="hidden" id="curl" name="curl" value="furl.html" />
+			<input type="hidden" id="key" name="key" value="'.$key.'" />
+			<input type="hidden" id="txnid" name="txnid" value="'.$_POST['txnid'].'" />
+			<input type="hidden" id="amount" name="amount" value="'.$_POST['amount'].'" />
+			<input type="hidden" id="productinfo" name="productinfo" value="'.$_POST['productinfo'].'" />
+			<input type="hidden" id="firstname" name="firstname" value="'.$_POST['firstname'].'" />
+			<input type="hidden" id="Lastname" name="Lastname" value="'.$_POST['Lastname'].'" />
+			
+			<input type="hidden" id="email" name="email" value="'.$_POST['email'].'" />
+			<input type="hidden" id="phone" name="phone" value="'.$_POST['phone'].'" />
+			
+	
+			<input type="hidden" id="Pg" name="Pg" value="'.$_POST['UPI'].'" />
+			<input type="hidden" id="hash" name="hash" value="'.$hash.'" />
+			</form>
+			<script type="text/javascript"><!--
+				document.getElementById("payment_form_submit").submit();	
+			//-->
+			</script>';
+	
+}
+ 
+//This function is for dynamically generating callback url to be postd to payment gateway. Payment response will be
+//posted back to this url. 
+function getCallbackUrl()
+{
+	$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+	$uri = str_replace('/index.php','/',$_SERVER['REQUEST_URI']);
+	return $protocol . $_SERVER['HTTP_HOST'] . $uri . 'response.php';
+}
+?>
---
+++ script.js
@@ -0,0 +1,145 @@
+var i = 0; 			
+var images = [];	
+var time = 3000;	
+	 
+images[0] = "images/11.jpg";
+images[1] = "images/12.jpg";
+images[2] = "images/13.jpg";
+
+function changeImg(){
+	document.slide.src = images[i];
+
+	if(i < images.length - 1){
+	  i++; 
+	} else { 
+	
+		i = 0;
+	}
+
+	
+	setTimeout("changeImg()", time);
+}
+
+window.onload=changeImg;
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+  if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked())
+}
+
+ function purchaseClicked() {
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+  }
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('This item is already added to the cart')
+            return
+        }
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+        var price = priceElement.innerHTML.slice(1);
+var quantity = quantityElement.value;
+console.log(price.slice(1));
+total = Number(total) + Number(Number(price) * Number(quantity));
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = total;
+    document.getElementById("btnClickedValue").value = total;
+    
+}
